3f7ceba33cc24fc9996491f2bd8d21d7dc696703
liangshuai <1589900136@qq.com>
2019-08-22 22:40:17 +0800

    consumer 和 function练习

---
+++ src/main/java/function和consume练习/DemoTest.java
@@ -0,0 +1,44 @@
+package function和consume练习;
+
+import com.google.common.cache.RemovalListener;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class DemoTest {
+
+    @Test
+    public void t1(){
+
+        Consumer listener = a -> System.out.println(a);
+
+        listener.accept(10);
+        Consumer<List<Integer>> modify = list ->
+        {
+            for (int i = 0; i < list.size(); i++)
+                list.set(i, 2 * list.get(i));
+        };
+
+        Consumer<List<Integer> >
+                dispList = list -> list.stream().forEach(a -> System.out.print(a + " "));
+
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(2);
+        list.add(1);
+        list.add(3);
+
+        // Implement modify using accept()
+        modify.accept(list);
+
+        // Implement dispList using accept()
+        dispList.accept(list);
+
+    }
+
+
+
+
+
+}
